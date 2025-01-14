4136d174f9ceb220a451f2b4d928df0a203df88e
youngeun0108 <duddmsakxm@naver.com>
2025-01-14 15:13:12 +0900

    Update color scheme

--- app/src/main/java/com/example/penguin/ui/theme/Color.kt
+++ app/src/main/java/com/example/penguin/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val black = Color(0xFF000000)
+val gray = Color(0xFF6E6E6E)
+val red = Color(0xFFFF0000)
+val yellow = Color(0xFFFACC2E)
